0a547dd1a41134eaf02c2aa8b3942cfd025fb4f3
jin <pji030225@naver.com>
2025-02-16 07:13:59 +0900

    2D Platformer

---
+++ 2D Platformer/EnemyMove.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+    BoxCollider2D boxCollider;
+    public int nextMove;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim= GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        Invoke("Think",5);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigid.velocity=new Vector2(nextMove, rigid.velocity.y);
+
+        //Platform Check
+        Vector2 frontVec = new Vector2(rigid.position.x+nextMove*0.2f, rigid.velocity.y);
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+        if (rayHit.collider == null)
+            Turn();
+    }
+
+    void Think()
+    {
+        //Set Next Active
+        nextMove = Random.Range(-1,2);
+
+        //Sprite Animation
+        anim.SetInteger("WalkSpeed", nextMove);
+
+        //Flip Sprite
+        if (nextMove != 0)
+            spriteRenderer.flipX = nextMove == 1;
+
+        //Recursive
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", nextThinkTime);
+    }
+
+    void Turn()
+    {
+        nextMove *= -1;
+        spriteRenderer.flipX = nextMove == 1;
+
+        CancelInvoke();
+        Invoke("Think", 5);
+    }
+
+    public void OnDamaged()
+    {
+        // Sprite Alpha
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+        // Sprite Flip Y
+        spriteRenderer.flipY = true;
+        // Collider Disable
+        boxCollider.enabled = false;
+        // Die Effect Jump
+        rigid.AddForce(Vector2.up*5, ForceMode2D.Impulse);
+        // Destroy
+        Invoke("DeActive", 5);
+    }
+
+    void DeActive()
+    {
+        gameObject.SetActive(false);
+    }
+}
